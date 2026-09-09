f10020e0490373e1fe8c0c750bcb558da900a2a8
Msspiccolo <msspiccolo1717@gmail.com>
2026-09-08 22:05:01 -0300

    Alteração user Git - Correção parametros

--- curso-react/src/pages/Auth.jsx
+++ curso-react/src/pages/Auth.jsx
@@ -26,7 +26,7 @@ function Auth() {
         if (user.password == pass){
             //mando para tela do painel
          localStorage.setItem("loggeded", JSON.stringify(user));
-            setMsg("Seja bem indo");
+            setMsg("Seja bem vindo!");
               nav('/painel');
 
         }else{
